c6c535189db6b75a116832d1a0001afcf9899655
hojat72elect <hojat72elect@gmail.com>
2024-11-01 04:30:04 -0400

    HeroLumber.java -> kt

---
+++ core/src/com/nopalsoft/zombiewars/objects/HeroLumber.kt
@@ -0,0 +1,18 @@
+package com.nopalsoft.zombiewars.objects
+
+import com.badlogic.gdx.physics.box2d.Body
+import com.nopalsoft.zombiewars.Assets
+
+class HeroLumber(body: Body) : Personajes(body) {
+    init {
+        DURATION_ATTACK = Assets.heroLumberShoot.animationDuration
+        DURATION_DEAD = Assets.heroForceDie.animationDuration + .2F
+        VELOCIDAD_WALK = 1F
+        DAMAGE = 1
+        DISTANCE_ATTACK = .5F
+        TIME_TO_ATTACK_AGAIN = 0F
+        vidas = 5
+        isFacingLeft = false
+        tipo = TIPO_NO_RANGO
+    }
+}
